635cfb3ba2a1b5b24c9b445175f86c47d84ab2d3
jananipriya1312 <janani.priyaeee@ksrce.ac.in>
2017-06-19 14:57:34 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,13 @@
+Public class odd{
+public Static void main(String[] args){
+int g;
+Scanner s=new Scanner();
+g=s.nextInt();
+if(g%2==0){
+System.out.println("even "+g);
+}
+else{
+System.out.println("odd"+g);
+}
+}
+}
