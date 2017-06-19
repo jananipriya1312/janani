e527cb773dd2b0f0bc9abec7d276f22391c8072f
jananipriya1312 <janani.priyaeee@ksrce.ac.in>
2017-06-19 14:51:28 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class leapyear {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arg;
+	        Scanner as=new Scanner(System.in);
+	        System.out.println("Enter the year");
+	        arg=as.nextInt();
+	       	        if(arg%4==0||arg%400==0){
+	        	 System.out.println("Leap year");
+	        }else{
+	        	 System.out.println("Not a leap year");
+	        }
+	        	
+	}
+
+}
