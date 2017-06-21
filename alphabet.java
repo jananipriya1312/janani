e3cf049e57837a046fa22aee9478f41fcbb0ffff
jananipriya1312 <janani.priyaeee@ksrce.ac.in>
2017-06-21 15:53:53 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,12 @@
+import java.io*;
+public class alpha{
+public static void main(String[] args){
+char a='j';
+if(a>='A'&&a<='Z'||a>='a'&&a<='z'){
+System.oout.println("alphabet");
+}
+else{
+System.out.println("special characters");
+}
+}
+}
