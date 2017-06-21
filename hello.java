ff50ae163ca4e740950f253592883aa1fa2c170e
jananipriya1312 <janani.priyaeee@ksrce.ac.in>
2017-06-21 14:39:07 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class hello{
+public static void main(String[] args){
+String a="hello";
+int b=a.length();
+String c=a.substring(0,b);
+for(int i=0;i<=5;i++){
+System.out.println(c);
+}
+}
+}
