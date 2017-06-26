3dc0c2cbc5a310e5894ff2ddad84506156af9fdc
jananipriya1312 <janani.priyaeee@ksrce.ac.in>
2017-06-26 09:55:14 +0530

    Add files via upload

---
+++ largesmall.java
@@ -0,0 +1,19 @@
+package largesmall;
+
+public class largesmall {
+
+	public static void main(String[] args) {
+	int a[]={23,56,78,45,13,77,35};
+	for(int i=0;i<=7;i++){
+		for(int j=i+1;j<=7;j++){
+			if(a[i]<=a[j]){
+				System.out.println("");
+				
+			
+			}
+		}
+	}
+
+	}
+
+}
