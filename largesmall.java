4874eb1e2f13f16c3b438338d54855cdc7ea8640
jananipriya1312 <janani.priyaeee@ksrce.ac.in>
2017-06-26 09:59:50 +0530

    Update largesmall.java

--- largesmall.java
+++ largesmall.java
@@ -3,17 +3,22 @@ package largesmall;
 public class largesmall {
 
 	public static void main(String[] args) {
-	int a[]={23,56,78,45,13,77,35};
-	for(int i=0;i<=7;i++){
-		for(int j=i+1;j<=7;j++){
-			if(a[i]<=a[j]){
-				System.out.println("");
+	int a=28;
+		int b=76;
+		int c=12;
+		if(a<b||a<c){
+				System.out.println(a);}
+		else if(b<c||b<a)
+		{
+			System.out.println(b);}
+		else{
+			System.out.println(c);}
 				
 			
-			}
-		}
+			
+		
 	}
 
 	}
 
-}
+
