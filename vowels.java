03c747dbf03834ce0f740ee6dcf8022c7aad324f
jananipriya1312 <janani.priyaeee@ksrce.ac.in>
2017-06-26 09:53:06 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,11 @@
+public class vowel{
+public static void main(String[] args){
+char a='j';
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'){
+System.out.println("vowels");
+}
+else{
+System.out.println("consonants");
+}
+}
+}
