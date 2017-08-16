0d29b114745d0f9b3f72ce774ac2fbb29197923b
jananipriya1312 <janani.priyaeee@ksrce.ac.in>
2017-08-16 10:16:43 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,21 @@
+    import java.util.Scanner;
+    public class Digit_Sum 
+    {
+    public static void main(String args[])
+    {
+    int m, n, sum = 0;
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter the number:");
+            m = s.nextInt();
+           for(i=0;i<m;i++){ 
+           while(m > 0){
+                n = m % 10;
+                sum = sum + n;
+                m = m / 10;
+            }
+            }
+            System.out.println("Sum of Digits:"+sum);
+
+        }
+
+    }
